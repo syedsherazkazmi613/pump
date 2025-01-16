7b2c2aeb9189393db3b9c8ccc855cba98b31e552
sherazkazmi613 <sherazkazmi613@gmail.com>
2025-01-16 08:44:04 -0800

    Initial commit: PUMP GROUP website

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB');
-        app.listen(process.env.PORT || 3000, () => {
+        app.listen(process.env.PORT , () => {
             console.log(`Server is running on port ${process.env.PORT || 3000}`);
         });
     })
